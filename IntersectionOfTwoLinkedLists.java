914b60da9ba534534e75a4fd02c97ae93c383a58
Jayaprakash-07-sys <vtu28024@veltech.edu.in>
2026-08-27 09:23:16 +0530

    Add files via upload

---
+++ IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,69 @@
+public class IntersectionOfTwoLinkedLists {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+
+        while (pointerA != pointerB) {
+
+            if (pointerA == null) {
+                pointerA = headB;
+            } else {
+                pointerA = pointerA.next;
+            }
+
+            if (pointerB == null) {
+                pointerB = headA;
+            } else {
+                pointerB = pointerB.next;
+            }
+        }
+
+        return pointerA;
+    }
+
+    public static void main(String[] args) {
+
+        // Create common part:
+        // 8 -> 4 -> 5
+        ListNode common = new ListNode(8);
+        common.next = new ListNode(4);
+        common.next.next = new ListNode(5);
+
+        // List A:
+        // 4 -> 1 -> 8 -> 4 -> 5
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = common;
+
+        // List B:
+        // 5 -> 6 -> 1 -> 8 -> 4 -> 5
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = common;
+
+        ListNode intersection = getIntersectionNode(headA, headB);
+
+        if (intersection != null) {
+            System.out.println("Intersection node: " + intersection.val);
+        } else {
+            System.out.println("No intersection");
+        }
+    }
+}
