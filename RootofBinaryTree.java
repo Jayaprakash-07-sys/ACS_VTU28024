903d0b4cbb685fca9ef5769c719bbbc2315259a8
Jayaprakash-07-sys <vtu28024@veltech.edu.in>
2026-09-03 10:30:13 +0530

    Add files via upload

---
+++ RootofBinaryTree.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class RootofBinaryTree {
+
+    public static boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    public static boolean isMirror(TreeNode p, TreeNode q) {
+
+        // Both nodes are null
+        if (p == null && q == null) {
+            return true;
+        }
+
+        // One node is null
+        if (p == null || q == null) {
+            return false;
+        }
+
+        // Values are different
+        if (p.val != q.val) {
+            return false;
+        }
+
+        // Compare opposite sides
+        return isMirror(p.left, q.right) &&
+               isMirror(p.right, q.left);
+    }
+
+    public static void main(String[] args) {
+
+        // Tree: [1,2,2,3,4,4,3]
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(isSymmetric(root));
+    }
+}
